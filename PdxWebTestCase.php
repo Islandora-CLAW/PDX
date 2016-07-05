a7638216dd0d4163e637ec84b9483be18f6c7c9c
Bryan Brown <bryjbrown@gmail.com>
2016-07-05 09:48:33 -0400

    #280: Adds test scaffolding and codecov.io integration

---
+++ PdxWebTestCase.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Islandora\Pdx;
+
+use Silex\WebTestCase;
+use Islandora\Chullo\Uuid\UuidGenerator;
+
+class PdxWebTestCase extends WebTestCase
+{
+  public function __construct()
+  {
+    
+  }
+
+  public function createApplication()
+  {
+
+  }
+
+
+}
